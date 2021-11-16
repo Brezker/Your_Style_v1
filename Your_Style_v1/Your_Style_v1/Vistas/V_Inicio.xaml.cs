8ef54369e089e5f47815b53c213c269270afb2fa
Brezker <2520360011rodriguez@gmail.com>
2021-11-16 01:10:08 -0600

    Agregar archivos de proyecto.

---
+++ Your_Style_v1/Your_Style_v1/Vistas/V_Inicio.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Your_Style_v1.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class V_Inicio : ContentPage
+    {
+        public V_Inicio()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Your_Style_v1/Your_Style_v1/Vistas/V_RegistrarCita.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Your_Style_v1.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class V_RegistrarCita : ContentPage
+    {
+        public V_RegistrarCita()
+        {
+            InitializeComponent();
+        }
+    }
+}
